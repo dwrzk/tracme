613823b43c3678c6936caa9b7133a900d548fd44
mnx464 <mnx464@ed5c02e7-e4ab-5615-a8ef-ad9e2b7edb50>
2012-12-02 01:07:44 +0000

    - Now stores all samples in memory and outputs it at the end - Adds 0 to absent APs in each scan and sorts the list - Added integer error checking for grid cell - Added CellSample and Sample classes for holding the sample set in memory - Fixed out of bounds error with the sample set

--- src/AirportScanner.java
+++ src/AirportScanner.java
@@ -27,36 +27,44 @@ public class AirportScanner extends WifiScanner
     */
    public ArrayList< AccessPoint > scan()
    {
-      StreamReader errorReader;
-      StreamReader outputReader;
-      ///System.out.println( "Attempting to run airport scanner..." );
+      StreamReader scanErrorReader;
+      StreamReader scanOutputReader;
+      StreamReader noiseOutputReader; // The output of the "airport -I" command which will give us the noise value.
+      System.out.println( "Running the airport scanner..." );
+
       // Clear the previous list so we can start fresh.
       apList.clear();
 
       try
       {
-         // Get the current runtime and execute the airport scanner process 
+         // Get the current runtime and execute the airport scanner processes.
          Runtime rt = Runtime.getRuntime();
-         Process proc = rt
-               .exec( "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s" );
+         Process scanProc = rt.exec( "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s" );
+         Process noiseProc = rt.exec( "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I" );
 
          // Read any error messages produced by the airport.
-         errorReader = new StreamReader( proc.getErrorStream() );
+         scanErrorReader = new StreamReader( scanProc.getErrorStream() );
 
          // Read the output produced by the airport scanner.
-         outputReader = new StreamReader( proc.getInputStream() );
+         scanOutputReader = new StreamReader( scanProc.getInputStream() );
+
+         // Read the output produced by the airport noise info.
+         noiseOutputReader = new StreamReader( noiseProc.getInputStream() );
 
          // Start both error and output threads.
-         errorReader.start();
-         outputReader.start();
+         scanErrorReader.start();
+         scanOutputReader.start();
+         noiseOutputReader.start();
 
          // Tell the main thread to wait for the threads to finish before continuing on with the analysis.
-         errorReader.join();
-         outputReader.join();
+         scanErrorReader.join();
+         scanOutputReader.join();
+         noiseOutputReader.join();
 
          // Check if the airport scanner encountered any errors.
-         int exitVal = proc.waitFor();
-         System.out.println( "Exit Value: " + exitVal );
+         int scanExitVal = scanProc.waitFor();
+         int noiseExitVal = noiseProc.waitFor();
+         System.out.println( "\"airport -s\" Exit Value: " + scanExitVal + "\n\"airport -I\" Exit Value: " + noiseExitVal );
 
       }
       catch( Throwable t )
@@ -67,7 +75,7 @@ public class AirportScanner extends WifiScanner
       }
 
       // Parse the returned string buffer so we can determine the AP information including the RSSI.
-      parseAirportStr( outputReader.getOutputStr() );
+      parseAirportStr( scanOutputReader.getOutputStr(), noiseOutputReader.getOutputStr() );
 
       // Return the complete list of access points that were scanned.
       return apList;
@@ -78,13 +86,39 @@ public class AirportScanner extends WifiScanner
     * Parses the airport scanner output line by line and gathers the data on the
     * AP.
     * 
-    * @param str
+    * @param scanStr
     *           The output of running the command "airport -s".
+    * @param infoStr
+    *           The output of running the command "airport -I".
     */
-   public void parseAirportStr( String str )
+   public void parseAirportStr( String scanStr, String infoStr )
    {
+      int agrCtlNoise = 0;
+
+      // Create a new scanner to parse the info string of the airport command.
+      Scanner infoScan = new Scanner( infoStr );
+
+      while( infoScan.hasNextLine() )
+      {
+         String lineStr = infoScan.nextLine();
+         Scanner lineScan = new Scanner( lineStr );
+
+         if( lineScan.hasNext() && lineScan.next().equals( "agrCtlNoise:" ) )
+         {
+            agrCtlNoise = lineScan.nextInt();
+            if( agrCtlNoise == 0 )
+            {
+               System.out.println( "Error: Must be connected to a wireless access point to calculate the SNR" );
+            }
+            break;
+         }
+      }
+
+      // Close the info scanner, because we are done getting the noise value.
+      infoScan.close();
+
       // Create a new scanner to parse the output string of the airport command.
-      Scanner sc = new Scanner( str );
+      Scanner sc = new Scanner( scanStr );
 
       // Parse through each line of the airport scan output.
       while( sc.hasNextLine() )
@@ -93,7 +127,7 @@ public class AirportScanner extends WifiScanner
          String lineStr = sc.nextLine();
          Scanner lineScan = new Scanner( lineStr );
 
-         ///System.out.println( lineStr );
+         System.out.println( lineStr );
 
          // If this line does not have any data to parse, skip to the next one.
          if( !lineScan.hasNext() )
@@ -103,27 +137,31 @@ public class AirportScanner extends WifiScanner
 
          String ssid = lineScan.next();
 
-         // Check what the token ////
-         if( ssid.equals( "SSID" ) || ssid.contains( "HP" ) )/////FIX ""
+         // Check if the token contains the SSID line or the IBSS networks and skip this lines, otherwise we parse the scanned APs.
+         if( ssid.equals( "SSID" ) )
          {
-            ///System.out.println( "Debug: Pattern not found in line - " + lineStr );
+            // Skip the data field info entries (usually the first line in the output).
             continue;
          }
          else if( lineStr.contains( "IBSS network" ) )
          {
             // This system does not support IBSS networks. Skip collecting any further data.
-            ///System.out.println( "Skipping IBSS networks" );
+            System.out.println( "Skipping IBSS networks" );
             break;
          }
          else
          {
             // Create a new access point and fill in the data from the current line.
             AccessPoint ap = new AccessPoint();
+
+            // We set the ID to -1 because this indicates that we need to map it into the table.
             ap.setID( -1 );
 
             String bssid = lineScan.next();
-            while( bssid.length() != 17 && bssid.charAt( 2 ) != ':' && bssid.charAt( 5 ) != ':'
-                  && bssid.charAt( 8 ) != ':' && bssid.charAt( 11 ) != ':' && bssid.charAt( 14 ) != ':' )
+
+            // Make sure that the BSSID is not the next word of the SSID. This can occur if the SSID is a multi-word name, so we need to keep reading the next token until we recognize the BSSID pattern.
+            while( bssid.length() != 17 || bssid.charAt( 2 ) != ':' || bssid.charAt( 5 ) != ':' || bssid.charAt( 8 ) != ':' || bssid.charAt( 11 ) != ':'
+                  || bssid.charAt( 14 ) != ':' )
             {
                ssid += bssid;
                bssid = lineScan.next();
@@ -131,7 +169,7 @@ public class AirportScanner extends WifiScanner
 
             ap.setSSID( ssid );
             ap.setBSSID( bssid );
-            ap.setRSSI( Math.abs( lineScan.nextInt() ) );
+            ap.setRSSI( RSSI_MAX + lineScan.nextInt() );
             ap.setChannel( lineScan.next() );
             ap.setHT( lineScan.next().equals( "Y" ) ? true : false );
             ap.setCC( lineScan.next() );
@@ -147,6 +185,7 @@ public class AirportScanner extends WifiScanner
       }
 
       sc.close();
-
    }
+
+   private static final int RSSI_MAX = 100; // The maximum RSSI value for the MAC airport utility, which is used for calculating the signal strength.
 }
